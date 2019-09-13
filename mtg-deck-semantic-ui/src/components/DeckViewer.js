f2213da2c105ad65cec28fa1845e735d7f4984c4
Thiago Medeiros <si-thiago@hotmail.com>
2019-09-13 01:00:28 -0300

    Restarting semantic-ui

--- mtg-deck-semantic-ui/src/components/DeckViewer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
-import {A} from 'hookrouter';
-
-const DeckViewer = () => <div><A href="/">return to App</A></div>
-
-export default DeckViewer
--- mtg-deck-semantic-ui/src/components/SearchField.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
-
-const SearchField = () => <Input placeholder='Search...' />
-
-export default SearchField
